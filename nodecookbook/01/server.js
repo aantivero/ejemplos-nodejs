d4fc3b675da637df6ae218912cfbf5c5b9a84d61
Alejandro Antivero <alejandro.antivero@gmail.com>
2014-08-02 15:38:52 -0300

    Enrutamiento utilizando querystring.

--- nodecookbook/01/server.js
+++ nodecookbook/01/server.js
@@ -2,27 +2,30 @@
  * Created by alex on 02/08/2014.
  */
 var http = require('http');
-var path = require('path');
+var url = require('url');
 
 //array con las rutas
 var pages = [
-    {route: '/', output: 'Wow Nodejs'},
-    {route: '/about', output: 'Otro ejemplo de enrutar con nodejs'},
-    {route: '/about/node', output: 'fullstack I/O basado en V8'},
-    {route: '/about/this', output: 'enrutamiento multilevel nodejs'},
-    {route: '/another page', output: function () {
+    {id: '1', route: '/', output: 'Wow Nodejs'},
+    {id: '2', route: '/about', output: 'Otro ejemplo de enrutar con nodejs'},
+    {id: '3', route: '/about/node', output: 'fullstack I/O basado en V8'},
+    {id: '4', route: '/about/this', output: 'enrutamiento multilevel nodejs'},
+    {id: '5', route: '/another page', output: function () {
         return 'Here\'s' + this.route;
     }}];
 
 http.createServer(function (request, response) {
-    var lookup = decodeURI(request.url);
+    //var lookup = decodeURI(request.url);
+    var id = url.parse(decodeURI(request.url), true).query.id;
     //vamos a recorrer el array hasta encontrar el correspondiente, caso contrario un 404
-    pages.forEach(function (page) {
-        if (page.route === lookup) {
-            response.writeHead(200, {'Content-Type' : 'text/html'});
-            response.end(typeof page.output === 'function' ? page.output() : page.output);
-        }
-    });
+    if (id) {
+        pages.forEach(function (page) {
+            if (page.id === id) {
+                response.writeHead(200, {'Content-Type' : 'text/html'});
+                response.end(typeof page.output === 'function' ? page.output() : page.output);
+            }
+        });
+    }
     if (!response.finished) {
         response.writeHead(404);
         response.end('No se encuentra la pagina.');
