318f50171830145cb57c4a8133d48ee738410ea1
Alejandro Antivero <alejandro.antivero@gmail.com>
2014-08-02 09:20:01 -0300

    Utilizar la libraria path para extraer la pagina requerida. Se agrega array con enrutamientos y 404 en caso de no encontrar el recurso.

--- nodecookbook/01/server.js
+++ nodecookbook/01/server.js
@@ -2,8 +2,27 @@
  * Created by alex on 02/08/2014.
  */
 var http = require('http');
+var path = require('path');
+
+//array con las rutas
+var pages = [
+    {route: '', output: 'Wow Nodejs'},
+    {route: 'about', output: 'Otro ejemplo de enrutar con nodejs'},
+    {route: 'another page', output: function () {
+        return 'Here\'s' + this.route;
+    }}];
 
 http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Wohoo!!');
+    var lookup = path.basename(decodeURI(request.url));
+    //vamos a recorrer el array hasta encontrar el correspondiente, caso contrario un 404
+    pages.forEach(function (page) {
+        if (page.route === lookup) {
+            response.writeHead(200, {'Content-Type' : 'text/html'});
+            response.end(typeof page.output === 'function' ? page.output() : page.output);
+        }
+    });
+    if (!response.finished) {
+        response.writeHead(404);
+        response.end('No se encuentra la pagina.');
+    }
 }).listen(8080);
