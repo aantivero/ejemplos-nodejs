9f9e7d001fcbed34c6b8d114d1a4631ac549b60c
Alejandro Antivero <alejandro.antivero@gmail.com>
2014-08-02 16:31:45 -0300

    Se agrego un array con los mimes types y verfica que el archivo exista y un read con su error y data

--- nodecookbook/01/serverstaticfile.js
+++ nodecookbook/01/serverstaticfile.js
@@ -16,6 +16,21 @@ http.createServer(function (request, response) {
     var lookup = path.basename(decodeURI(request.url)) || 'index.html';
     var f = 'content/' + lookup;
     fs.exists(f, function (exists) {
-       console.log(exists ? lookup + " is there." : lookup + " doesn't exist.");
+       if (exists) {
+           fs.readFile(f, function (err, data) {
+               if (err) {
+                   response.writeHead(500);
+                   response.end("Error en el servidor!");
+                   return;
+               }
+               var headers = {'Content-Type' : mimeTypes[path.extname(lookup)]};
+               response.writeHead(200, headers);
+               response.end(data);
+           });
+           return;
+       }
+       response.writeHead(404); //no encuentra el archivo
+       response.end("No se encuentra el archivo");
     });
 }).listen(8080);
+console.log("Iniciado el servidor de archivos estaticos.")
