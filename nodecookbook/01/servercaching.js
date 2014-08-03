a752f2748fcfb26736634c956a77222db0abb0ca
Alejandro Antivero <alejandro.antivero@gmail.com>
2014-08-02 21:06:56 -0300

    Agrego dentro de la funcion de caching fs.stats que sirve para determinar si el archivo fue modificado, leyendolo nuevamente si es modificado.

--- nodecookbook/01/servercaching.js
+++ nodecookbook/01/servercaching.js
@@ -16,17 +16,29 @@ var cache = {};
 console.log("pasa a");
 
 function cacheAndDeliver(f, cb) {
-    if (!cache[f]) {
-        fs.readFile(f, function (err, data) {
-           if (!err) {
-               cache[f] = {content: data};
-           }
-           cb(err, data);
-        });
-        return;
-    }
-    console.log('cargando ' + f + ' del cache');
-    cb(null, cache[f].content);
+    //esto me dice en que estado esta el archivo, si fue modificado, etc
+    /* atime ultimo acceso, mtime ultima modificacion, ctime ultimo cambio
+     * mtime registra alteraciones en el contenido del archivo y ctime alteraciones del archivo.
+     * si cambio permisos del archivo se modifica ctime pero mtime queda igual
+     */
+    fs.stat(f, function (err, stats) {
+        if (err) { console.log('Hubo un error!!!', err); }
+        //para que funcione en windows hay que usar mtime en lugar de ctime
+        var lastChanged = Date.parse(stats.mtime);
+        var isUpdated = (cache[f]) && lastChanged > cache[f].timestamp;
+        if (!cache[f] || isUpdated) {
+            fs.readFile(f, function (err, data) {
+                console.log('Leyendo archivo ' + f);
+                if (!err) {
+                    cache[f] = {content: data, timestamp: Date.now()};
+                }
+                cb(err, data);
+            });
+            return;
+        }
+        console.log('cargando ' + f + ' del cache');
+        cb(null, cache[f].content);
+    });
 }
 
 console.log("pasa b");
