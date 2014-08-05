8112502e0d3f0636527240159381c97a331e2abf
Alejandro Antivero <alejandro.antivero@gmail.com>
2014-08-05 10:03:02 -0300

    Maximo de tamanio en el post data. Prevenimos que el usuario nos envie mas data y nos produzca un error en el servidor

--- nodecookbook/02/processing_post_data/server.js
+++ nodecookbook/02/processing_post_data/server.js
@@ -3,6 +3,8 @@ var form = require('fs').readFileSync('form.html');
 //convertir el row data en un object con el metodo parse
 var querystring = require('querystring');
 var util = require('util');
+//agregamos un maximo del tamanio que se puede enviar por post
+var maxData = 2 * 1024 * 1024;//2mb
 
 http.createServer(function (request, response) {
    if (request.method === 'GET') {
@@ -13,10 +15,21 @@ http.createServer(function (request, response) {
        var postData = '';
        request.on('data', function (chunk) {
            postData += chunk;
+           if (postData.length > maxData) {
+               postData = '';
+               this.destroy();//prevenimos que el usuario no mande mas data
+               response.writeHead(413);//request entity too large
+               response.end('Demasiado largo');
+           }
        }).on('end', function () {
-          var postDataObject = querystring.parse(postData);
-          console.log('Los datos recibidos son : '+postData);
-          response.end('Datos recibidos:\n' + util.inspect(postDataObject));
+           if (!postData) { 
+               //prevencion frente a un postdata vacio
+               response.end();
+               return;
+           }
+           var postDataObject = querystring.parse(postData);
+           console.log('Los datos recibidos son : '+postData);
+           response.end('Datos recibidos:\n' + util.inspect(postDataObject));
        });
    }
 }).listen(8080);
