4f806c424a6fd379cd620e6c95e74f2622a82e00
Alejandro Antivero <alejandro.antivero@gmail.com>
2014-08-05 09:24:04 -0300

    Parsear con querystring un raw data en un object

--- nodecookbook/02/processing_post_data/server.js
+++ nodecookbook/02/processing_post_data/server.js
@@ -1,5 +1,8 @@
 var http = require('http');
 var form = require('fs').readFileSync('form.html');
+//convertir el row data en un object con el metodo parse
+var querystring = require('querystring');
+var util = require('util');
 
 http.createServer(function (request, response) {
    if (request.method === 'GET') {
@@ -11,8 +14,9 @@ http.createServer(function (request, response) {
        request.on('data', function (chunk) {
            postData += chunk;
        }).on('end', function () {
+          var postDataObject = querystring.parse(postData);
           console.log('Los datos recibidos son : '+postData);
-          response.end('Datos recibidos:\n' + postData);
+          response.end('Datos recibidos:\n' + util.inspect(postDataObject));
        });
    }
 }).listen(8080);
