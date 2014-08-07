550fd9d3e306556dd3cdeb2e9af1e51ee6b4faa5
Alejandro Antivero <alejandro.antivero@gmail.com>
2014-08-06 22:35:23 -0300

    En esta caso la libreria formidable no solo maneja archivos sino también fields

--- nodecookbook/02/handling_file_uploads/server.js
+++ nodecookbook/02/handling_file_uploads/server.js
@@ -11,14 +11,23 @@ http.createServer(function (request, response) {
    if (request.method === 'POST') {
        var incoming = new formidable.IncomingForm();
        incoming.uploadDir = 'uploads';
-       incoming.on('file', function (field, file) {
-           if (!file.size) {
-               return;
-           }
-           response.write(file.name + ' recibido\n');
-       }).on('end', function () {
-           response.end('Todos los archivos fueron recibidos');
-       });
+       incoming.on('fileBegin', function (field, file) {
+            //esto preserva el nombre del archivo porque formidable genera un nombre aleatorio hexadecimal
+               if (file.name) {
+                   file.path += "-" + file.name;
+               }
+           }).on('file', function (field, file) {
+               if (!file.size) {
+                   return;
+               }
+               response.write(file.name + ' recibido\n');
+           }).on('field', function (field, value){
+               //maneja entrada de un formulario no necesariamente archivos
+               response.write(field + ' : ' + value + '\n');
+           }).on('end', function () {
+               response.end('Todos los archivos fueron recibidos');
+           });
+       }
        incoming.parse(request);
    }
    if (request.method === 'GET') {
@@ -27,5 +36,5 @@ http.createServer(function (request, response) {
    }
 }).listen(8080);
 
-console.log('Iniciado el servidor con upload de archivos');
+console.log('Iniciado el servidor con upload de archivos y datos en el puerto 8080');
 
